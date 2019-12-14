53736714c0517ea1bed28137491c4f51c1b1acb2
Anwesh43 <anweshthecool0@gmail.com>
2019-12-15 03:18:55 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -13,3 +13,25 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawBouncyRect(context : CanvasRenderingContext2D, scale : number, size : number, gap : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const y : number = size * sf
+        context.save()
+        context.translate(gap * scale, 0)
+        context.fillRect(-size / 2, -size / 2 - y / 2, size, size / 2 + y / 2)
+        context.restore()
+    }
+
+    static drawUDBRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const gap : number = w / (nodes + 1)
+        const size : number = gap / sizeFactor
+        context.fillStyle = foreColor 
+        context.save()
+        context.translate(gap * (i + 1), h / 2)
+        DrawingUtil.drawBouncyRect(context, scale, size, gap)
+        context.restore()
+    }
+}
