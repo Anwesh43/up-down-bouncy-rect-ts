3798283f44d0830dec6be4bfd71ad81c4798d2bc
Anwesh43 <anweshthecool0@gmail.com>
2019-12-15 03:29:25 +0530

    adding UpDownBouncyRect class

--- index.ts
+++ index.ts
@@ -150,6 +150,31 @@ class UDBRNode {
             return curr
         }
         cb()
-        return this 
+        return this
+    }
+}
+
+class UpDownBouncyRect {
+
+    curr : UDBRNode = new UDBRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        if (this.curr) {
+            this.curr.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
     }
 }
